85c2a107470cc2d564522ef72852db4d2cc18cde
Kai Jellinghaus <contact@kaij.tech>
2022-07-21 14:15:20 +0200

    Create root release profile

--- mix.exs
+++ mix.exs
@@ -6,7 +6,15 @@ defmodule Leagueml.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        league_ml: [
+          applications: [
+            graphql_api: :permanent,
+            prometheus_exporter: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
